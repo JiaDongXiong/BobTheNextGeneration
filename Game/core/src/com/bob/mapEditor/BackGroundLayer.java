8b0f0c03c1c05f8ed3e3e1c86618eeeca130aaea
JiaDongXiong <842785545@qq.com>
2017-06-04 23:53:37 +0100

    Upload the lastest release version

--- Game/core/src/com/bob/mapEditor/BackGroundLayer.java
+++ Game/core/src/com/bob/mapEditor/BackGroundLayer.java
@@ -8,7 +8,7 @@ import com.bob.main.TextureFactory;
 public class BackGroundLayer extends Layer {
 	
 	public BackGroundLayer(Skin skin) {
-		Image foreground = new Image(TextureFactory.createTexture("screens/editorBkg.png"));
+		Image foreground = new Image(TextureFactory.createTexture("screens/03.png"));
         group.addActor(foreground);
 	}
 	
